215b57f10d34187af346ed9850fcf23f59fa85d5
unknown <DerikWandStudio@gmail.com>
2026-09-26 19:30:08 +0330

    1.4

--- js/gallery-data.js
+++ js/gallery-data.js
@@ -8,19 +8,11 @@ const galleryItems = [
         alt: ""
     },
     {
-        image: "images/gallery/coach1035.webp",
-        title: "بابک مدنی در سال 1404",
-        description: "تصویری از مربی رویای افلاک، بابک مدنی، در حین تمرین",
-        date: "20 آبان 1394",
-        category: "events",    // training | matches | teams | events
-        alt: ""
+        image: "images/gallery/1405.07.04.01.webp",
+        title: "کلاس تئوری 4 مهرماه 1404",
+        description: "تصویری از بازیکنان نونهالان در کلاس تئوری",
+        date: "04 مهر 1405",
+        category: "events",
+        alt: "تصویری از بازیکنان نونهالان در کلاس تئوری",
     },
-    {
-        image: "images/gallery/coach1035.webp",
-        title: "بابک مدنی در سال 1394",
-        description: "تصویری از مربی رویای افلاک، بابک مدنی، در حین تمرین",
-        date: "20 آبان 1394",
-        category: "training",    // training | matches | teams | events
-        alt: ""
-    }
 ];
